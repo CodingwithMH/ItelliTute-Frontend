9796a1aac9a2f6a5a7e671c32cc628cda4d55d94
Muhammad <Hammad90191@gmail.com>
2025-08-22 20:11:40 +0500

    Updated loading

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -6,22 +6,31 @@ import { useNavigate } from 'react-router-dom';
 import { fetchUser } from '../store/user/userSlice';
 
 const Login = () => {
-  const {userDetails}=useSelector(state=>state.user);
+  const {userDetails,loading}=useSelector(state=>state.user);
   const dispatch=useDispatch();
   const navigate=useNavigate();
   useEffect(()=>{
     dispatch(fetchUser());
   },[dispatch])
+  if (loading) {
+    return (
+      <div className="min-h-screen flex justify-center items-center bg-gradient-to-b from-[#000000] via-[#1a0b2e] to-[#2d1b69]">
+        <div className="loader2"></div>
+      </div>
+    );
+  }
+
+  if (userDetails) {
+    navigate('/');
+    return null;
+  }
   return (
     <>
-    {!userDetails ?
-
       <div className="min-h-screen w-full relative overflow-hidden bg-gradient-to-b from-[#000000] via-[#1a0b2e] to-[#2d1b69] flex justify-center items-center">
       {/* Floating spheres */}
       <Spheres/>
       <LoginForm/>
-      </div>:navigate('/')
-      }
+      </div>
     </>
   )
 }
